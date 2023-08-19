8333b545b463b5b5c0215ef951c3bb8afdb326e5
Sushil Mishra <sushilmishra9699@gmail.com>
2023-08-19 18:42:37 +0530

    learning with codeingcamp

--- src/main/java/com/sushil/LearningSpring/student/StudentController.java
+++ src/main/java/com/sushil/LearningSpring/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.sushil.LearningSpring.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/sushil/LearningSpring/student/StudentService.java
+++ src/main/java/com/sushil/LearningSpring/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.sushil.LearningSpring.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudent(){
         return List.of(new Student(
